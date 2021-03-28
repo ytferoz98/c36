43d831b3905d61cfb076837893e5cd7e2c11c97d
ytferoz98 <75500686+ytferoz98@users.noreply.github.com>
2021-03-28 12:41:57 +0100

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,24 @@
+class Form {
+    constructor(){}
+    display(){
+        var title=createElement('h2')
+        title.html('car racing game')
+        title.position(130,0)
+        var input=createInput('name')
+        var button=createButton('play')
+        var greeting=createElement('h3')
+        input.position(130,60)
+        button.position(250,200)
+        button.mousePressed(function(){
+            input.hide()
+            button.hide()
+            var name=input.value()
+            playerCount=playerCount+1
+            player.update(name)
+            player.updateCount(playerCount)
+            greeting.html('hello'+name)
+            greeting.position(130,160)
+        })
+        
+    }
+}
